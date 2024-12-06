eb7d32cc9d20348d996cea596f578d3698cc746d
waelhym <37433514+waelhym@users.noreply.github.com>
2024-12-06 18:12:52 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ PORT=$2
 CHAT=$3
 
 # Clone Odoo directory
-git clone --depth=1 https://github.com/minhng92/odoo-18-docker-compose $DESTINATION
+git clone --depth=1 https://github.com/waelhym/odoo18-docker-compose $DESTINATION
 rm -rf $DESTINATION/.git
 
 # Create PostgreSQL directory
@@ -46,4 +46,4 @@ find $DESTINATION -type d -exec chmod 755 {} \;
 # Run Odoo
 docker-compose -f $DESTINATION/docker-compose.yml up -d
 
-echo "Odoo started at http://localhost:$PORT | Master Password: minhng.info | Live chat port: $CHAT"
+echo "Odoo started at http://localhost:$PORT | Master Password: Admin098765@ | Live chat port: $CHAT"
